9380f67a2df52e169377a9813ee87db48b3ac0c0
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-03-01 17:11:59 +0500

    don't use dnsResolve in pacScript

--- src/background/pacScript.js
+++ src/background/pacScript.js
@@ -1,9 +1,9 @@
 function getProxyById(proxies, id) {
   const proxy = proxies.filter(p => p.id === id)[0]
 
-  return proxy.id === 'direct'
-    ? `'DIRECT'`
-    : `'${
+  return proxy.id === 'direct' ?
+    `'DIRECT'` :
+    `'${
         proxy.protocol.toLowerCase().indexOf('socks') > -1
           ? proxy.protocol
           : 'PROXY'
@@ -11,16 +11,17 @@ function getProxyById(proxies, id) {
 }
 
 module.exports.pacScriptData = preferences => {
-  const { domainProxyList, proxies, defaultProxy } = preferences
+  const {
+    domainProxyList,
+    proxies,
+    defaultProxy
+  } = preferences
   const domains = Object.keys(domainProxyList)
   let func = 'function FindProxyForURL(url, host) {\n'
 
-  func += `if (isPlainHostName(host) ||
-        shExpMatch(host, "*.local") ||
-        isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
-        isInNet(dnsResolve(host), "172.16.0.0",  "255.240.0.0") ||
-        isInNet(dnsResolve(host), "192.168.0.0",  "255.255.0.0") ||
-        isInNet(dnsResolve(host), "127.0.0.0", "255.255.255.0"))
+  func +=
+    `if (isPlainHostName(host) ||
+        shExpMatch(host, "*.local"))
         return "DIRECT"\n`
 
   domains.forEach(domain => {
